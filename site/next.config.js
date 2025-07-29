d29ecdd12d5f95b71fc4be77d27d4f99ae984095
KWRFS <arsta@danskebank.lt>
2025-07-29 08:28:32 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     name: 'KASM WORKSPACES REPO FOR SOC',
     description: 'SPAR - SOC Platform for Asset Repository',
     icon: 'https://github.com/KWRFS/SPAR/blob/84aa9f9c577ddd0976047645a6fc7b9353c414b6/Untitled%20design.png',
-    listUrl: 'https://github.com/KWRFS/SPAR/',
+    listUrl: 'https://kwrfs.github.io/SPAR/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
